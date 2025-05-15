50fc5089127547bdd1f29bf1d7107e914a2bf5e5
Cristian Acevedo <crashedthe@gmail.com>
2025-05-15 08:25:37 -0500

    Carpeta CSS

---
+++ gestor_tareas/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Index </title>
+</head>
+    <body>
+        <h1> Iniciar Sesion </h1>
+        <form action="#" method="post">
+            <label> Nombre </label>
+            <input type="text" name="name">
+            <br>
+            <label> Contraseña </label>
+            <input type="password" name="password">
+            <input type="submit" value="enviar">
+        </form>
+    </body>
+</html>
