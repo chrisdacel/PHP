30069923331e7206f35afb5a2be45c1504ff2809
Joalregra <johanalexis808@gmail.com>
2025-05-15 11:09:38 -0500

    funcionalidad total registro

--- gestor_tareas/registro.php
+++ gestor_tareas/registro.php
@@ -2,7 +2,8 @@
 include_once"includes/header.php";
 ?>
 <?php
-// include_once("header.php");
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+
 $user_name=$_POST["user_name"];
 $archivo="usuarios/". $user_name.".json";
 
@@ -17,10 +18,16 @@ $tareas=array("tareas"=>array());
     tareas=[];
     ]*/
 
+// ESTRUCTURA TEMPORAL(Internet)
+// conversion a cadena de json
 $json=json_encode($tareas,JSON_PRETTY_PRINT);
+// creacion de archivo y envio de informacion
+file_put_contents($archivo,$json);
 }
-file_put_contents($tareas,$json);
+
 echo("se ha registrado correctamente");
+
+}
 
 ?>
     <h1>Registrate</h1>
@@ -30,10 +37,6 @@ echo("se ha registrado correctamente");
         <label>Contraseña:</label>
         <input type="password" placeholder="Contraseña" required>
         <input type="submit" value="enviar">
-
-<?php
-// include_once("footer.php");
-?>
 
 <?php
     include_once"includes/footer.php";
